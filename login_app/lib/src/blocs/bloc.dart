51e6e63cad22cc0718655645221619ad89880626
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-01 15:10:20 +0200

    cleaning the stream and applying transform

--- login_app/lib/src/blocs/bloc.dart
+++ login_app/lib/src/blocs/bloc.dart
@@ -3,9 +3,14 @@ import 'validators.dart';
 class Bloc with Validators{
   final _email = StreamController<String>();
   final _pass = StreamController<String>();
-  Stream<String> get Email => _email.stream;
-  Stream<String> get password => _pass.stream;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _pass.stream.transform(validatePassword);
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _pass.sink.add;
+
+  dispose(){
+    _email.close();
+    _pass.close();
+  }
 }
