5d9b3d414bca5d4c06ec06dd1a1755d4c610a6e8
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-01 14:59:14 +0200

    adding validator mixin

--- login_app/lib/src/blocs/bloc.dart
+++ login_app/lib/src/blocs/bloc.dart
@@ -1,5 +1,11 @@
 import 'dart:async';
-class Bloc {
-  final emailController = StreamController<String>();
-  final passController = StreamController<String>();
+import 'validators.dart';
+class Bloc with Validators{
+  final _email = StreamController<String>();
+  final _pass = StreamController<String>();
+  Stream<String> get Email => _email.stream;
+  Stream<String> get password => _pass.stream;
+
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _pass.sink.add;
 }
--- login_app/lib/src/screens/login_screen.dart
+++ login_app/lib/src/screens/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../blocs/bloc.dart';
 class LoginScreen extends StatelessWidget{
   Widget build(context){
     return Container(
