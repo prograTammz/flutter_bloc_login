0408eee94a148cc75756e528d31041db21cc203b
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-01 12:36:34 +0200

    small bug fix (missing return statement)

--- login_app/lib/src/screens/login_screen.dart
+++ login_app/lib/src/screens/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget{
   Widget build(context){
-    Container(
+    return Container(
       margin: EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
