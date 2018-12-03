5e8361b9f1e825bed5e535fc5597a5ddfffb1237
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-03 11:40:43 +0200

    adding rx libarary and using stream combiner

--- login_app/lib/src/blocs/bloc.dart
+++ login_app/lib/src/blocs/bloc.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
+
 class Bloc with Validators{
   final _email = StreamController<String>();
   final _pass = StreamController<String>();
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _pass.stream.transform(validatePassword);
 
+  Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e,p)=> true);
+
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _pass.sink.add;
+
 
   dispose(){
     _email.close();
